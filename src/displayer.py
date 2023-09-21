953e038ce35b7ef065a35c6d324aad385ce67ac8
OneWinged-ShunKaido <47723046+OneWinged-ShunKaido@users.noreply.github.com>
2023-09-21 19:39:36 +0200

    Create displayer.py

---
+++ src/displayer.py
@@ -0,0 +1,35 @@
+from os import system
+
+class Displayer:
+    display_success: bool = False
+    display_error: bool = True
+    last_func_name: str
+    method: str
+    args: dict = dict.fromkeys(
+        [
+            "base",
+            "address",
+            "offsets",
+            "final_offset",
+            "value"
+        ])
+    exception: str
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def on_clear():
+        system("cls")
+
+    def on_success(self, ctx):
+        print(f"[*] Successfully called {self.last_func_name} -> {ctx}")
+
+    def on_error(self, e: Exception):
+        print(f"At {self.last_func_name} [{self.method}] ({e})")
+        for key, value in self.args.items():
+            print(f"{key} -> {value}")
+
+    @staticmethod
+    def on_w_type(e):
+        print(f"[!] Type not supported for \"{type(e)}\"")
